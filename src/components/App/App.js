65f740d955064563812d4fa9dd8176e589839f35
Hanna <hannaeberhoefer@yahoo.com>
2021-07-03 10:56:13 +0100

    Adjust App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import './App.css';
+import {getCurrentWeather} from '../../api/OpenWeather';
 import CurrentWeather from '../CurrentWeather/CurrentWeather';
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            currentWeather: [],
+        }
+    }
+
+    componentDidMount() {
+        const current = getCurrentWeather();
+        this.setState({currentWeather: current});
+    }
+
     render() {
         return (
             <div className='App'>
-                <CurrentWeather />
+                <CurrentWeather condition={this.state.currentWeather} />
             </div>
         )
     }
